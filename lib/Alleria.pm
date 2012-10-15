0a5ba0b5a093eee01eb0208dce531501c319959e
n4kz <nfokz@yandex.ru>
2012-10-16 00:23:00 +0400

    Libraries

---
+++ lib/Alleria.pm
@@ -0,0 +1,123 @@
+package Alleria;
+use Alleria::Core 'base', 'has';
+use Term::ReadKey;
+use Net::Jabber 'client';
+my %events;
+
+sub events { \%events }
+
+has qw{
+	debug host port tls timeout client jid
+	ok logfile
+	username password resource
+};
+
+sub new {
+	my $self = SUPER::new { shift } @_;
+
+	unless ($self->{'password'}) {
+		print "Password:";
+		ReadMode 2;
+		chomp($self->{'password'} = readline STDIN);
+		ReadMode 0;
+		print "\n";
+	}
+
+	$self->{'ok'}         = 1;
+	$self->{'debug'}    ||= 0;
+	$self->{'tls'}      ||= 0;
+	$self->{'timeout'}  ||= 1;
+	$self->{'port'}     ||= 5222;
+	$self->{'resource'} ||= 'Alleria';
+	$self->{'logfile'}  ||= '/dev/null';
+
+	return $self;
+} # new
+
+sub start {
+	my ($self) = @_;
+
+	$self->client(
+		Net::Jabber::Client->new(
+			debuglevel => $self->debug(), 
+			debugfile  => 'stdout',
+		)
+	);
+
+	$self->Connect(
+		hostname => $self->host(),
+		port     => $self->port(),
+		timeout  => $self->timeout(),
+		tls      => $self->tls(),
+	) or do {
+		$self->fire('error', ['Connection error: '. $!]);
+		return $self;
+	}; 
+
+	$self->PresenceDB();
+	$self->RosterDB();
+
+	$self->SetCallBacks(
+		presence => sub { $self->fire(presence => [pop]) },
+		message  => sub { $self->fire(message  => [pop]) },
+		iq       => sub { $self->fire(iq       => [pop]) },
+	);
+
+	sleep 1;
+
+	my @auth = $self->AuthSend(
+		username => $self->username(),
+		password => $self->password(),
+		resource => $self->resource(),
+	);
+	
+	if ($auth[0] eq 'ok') {
+		$self->RosterRequest();
+		$self->PresenceSend();
+		$self->RosterGet();
+
+		$self->fire('connect');
+	} else {
+		$self->fire('error', ['Authentication failed', @auth]);
+	}
+
+	return $self;
+} # start
+
+sub stop {
+	my ($self) = (@_);
+
+	$self->Disconnect();
+	$self->fire('disconnect');
+
+	return $self;
+} # stop
+
+sub process {
+	my ($self) = (@_);
+
+	if ($self->Connected()) {
+		$self->Process() // $self->disconnect;
+	} else {
+		$self->connect();
+	}
+
+	return $self;
+} # process
+
+sub AUTOLOAD {
+	my $method = our $AUTOLOAD;
+	$method =~ s{^.*::} {};
+
+	return if $method eq 'DESTROY';
+
+	return unless ref $_[0];
+	my $client = shift->client();
+
+	$method = UNIVERSAL::can($client, $method);
+	return unless $method;
+
+	return $method->($client, @_);
+} # AUTOLOAD
+
+1;
---
+++ lib/Alleria/Core.pm
@@ -0,0 +1,202 @@
+package Alleria::Core;
+use strict;
+use warnings;
+use feature ':5.12';
+use utf8;
+
+my (%shackled, %events, %plugins);
+
+sub events { \%events }
+
+# Alleria::Core->new(key => value, ...)
+# Alleria::Core::new('Alleria', key => 'value', ...)
+sub new (;$@) {
+	my $invocant = ref $_[0] || $_[0] || caller || __PACKAGE__;
+
+	return bless
+		{
+			@_[1 .. @_ - 1],
+			@_ % 2? () : (undef)
+		},
+		$invocant;
+} # new
+
+# $self->extend(method => sub {});
+sub extend ($$$) {
+	my $slot = join '::', ref $_[0] || $_[0], $_[1];
+
+	no strict 'refs';
+	*$slot = $_[2];
+
+	return $_[0];
+} # extend
+
+# $self->inherit('Package::Test', 'Test::Package', ...);
+sub inherit ($@) {
+	no strict 'refs';
+	my $ISA = join '::', ref $_[0] || $_[0], 'ISA';
+	push @$ISA, @_[1 .. @_ - 1];
+} # inherit
+
+# $self->properties($name, ...)
+sub properties ($@) {
+	my $package;
+
+	foreach my $field (@_) {
+		unless ($package) {
+			$package = ref $_[0] || $_[0];
+			next;
+		}
+
+		my $slot   = join '::', $package, $field;
+		my $method = sub {
+			return $_[0]->{$field} = $_[1] if @_ > 1;
+			return $_[0]->{$field};
+		};
+
+		no strict 'refs';
+		*$slot = $method;
+	}
+
+	return $_[0];
+} # properties
+
+# has 'name', 'length', ...;
+# properties alias
+sub has (@) {
+	my $caller = caller;
+	return unless $caller;
+
+	properties $caller, @_; 
+} # has
+
+# $self->clear($method, ...)
+sub clear ($@) {
+	my $package;
+
+	foreach (@_) {
+		unless ($package) {
+			$package = ref $_[0] || $_[0];
+			next;
+		}
+
+		my $slot = join '::', $package, $_;
+
+		no strict 'refs';
+
+		undef *$slot;
+
+		if (exists $shackled{$slot}) {
+			*$slot = delete $shackled{$slot};
+			         delete $shackled{join '::', $slot, '[]'};
+		}
+	}
+
+	return $_[0];
+} # clear
+
+#         $self->shackleshot(method => sub {}, $prepend)
+# Alleria::Core->shackleshot(method => sub {}, $prepend)
+sub shackleshot ($$$$) {
+	my $package = ref $_[0] || $_[0];
+	my $slot    = join '::', $package, $_[1];
+	my $key     = join '::', $slot,  '[]';
+	my $chain   = $shackled{$key} ||= [];
+
+	unless (@$chain) {
+		my $coderef = UNIVERSAL::can($package, $_[1]);
+		die 'Not a code reference' unless ref $coderef eq 'CODE';
+
+		push @$chain, $shackled{$slot} = $coderef;
+
+		# Wrap old method
+		my $method = sub {
+			my ($post, @result);
+
+			foreach (@$chain) {
+				if ($_ == $coderef) {
+					# Call method in right context
+					if (wantarray) {
+						@result = &$_;
+					} else {
+						$result[0] = &$_;
+					}
+					$post = 1;
+				} else {
+					# Call hooks 
+					last unless $_->($slot, \@_, \@result) or $post;
+				}
+			}
+
+			return wantarray? @result : $result[0];
+		};
+
+		no strict 'refs';
+		no warnings 'prototype';
+		*$slot = $method;
+	}
+
+	if ($_[3]) {
+		unshift @$chain, $_[2];
+	} else {
+		push @$chain, $_[2];
+	}
+
+	return $_[0];
+} # shackleshot
+
+#         $self->load('plugins/test.pl');
+# Alleria::Core->load('plugins/test.pl', ...);
+# Alleria::Core->load('test');
+sub load ($@) {
+	foreach (@_[1 .. $#_]) {
+		local $_ = $_;
+		s{^\.*/} {}g;
+		s{^} {plugins/} unless m{^plugins};
+		s{$} {.pl} unless m{\.p[lm]$};
+
+		$plugins{$_} ||= eval "require '$_'" or die $@;
+	}
+
+	return $_[0];
+} # load
+
+#         $self->fire('test', [1, 2, 3]);
+# Alleria::Core->fire('test', [1, 2, 3]);
+sub fire ($$$) {
+	&$_ foreach @{ $_[0]->events->{$_[1]} ||= [] };
+
+	return $_[0];
+} # fire
+
+#         $self->focus('test', sub {});
+#         $self->focus(['test', 't2'], sub {});
+# Alleria::Core->focus('test', sub {});
+sub focus ($$$) {
+	foreach (ref $_[1]? @{ $_[1] } : $_[1]) {
+		push @{
+			$_[0]->events->{$_} ||= []
+		}, $_[2];
+	}
+
+	return $_[0];
+} # focus
+
+# use Alleria::Core 'base', 'strict', 'has'
+sub import {
+	return unless @_ > 1;
+
+	foreach (@_[1 .. @_ - 1]) {
+		inherit scalar caller, __PACKAGE__
+			if m{base};
+
+		extend scalar caller, 'has' => \&has
+			if m{has};
+	}
+
+	strict->import();
+	warnings->import();
+	feature->import(':5.12');
+} # import
+
+1;
