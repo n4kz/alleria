f9d361bb23e369ecc08449f0f6afbbe2db8103f1
n4kz <nfokz@yandex.ru>
2012-10-18 22:50:55 +0400

    Subscribe and unsubscribe methods moved from presence to subscription plugin

--- plugins/presence.pl
+++ plugins/presence.pl
@@ -2,6 +2,12 @@ use Alleria::Core 'strict';
 
 my %fields = map { ("Get$_" => lc) } qw{ To From Type Status Show };
 
+Alleria->focus(connect => sub {
+	my ($self, $event, $args) = @_;
+
+	$self->PresenceDB();
+});
+
 Alleria->focus(presence => sub {
 	my ($self, $event, $args) = @_;
 	my $presence = $args->[0];
@@ -28,38 +34,6 @@ Alleria->focus(presence => sub {
 	}
 
 	$self->fire($event, [\%presence]) if defined $event;
-});
-
-Alleria->extend(subscribe => sub {
-	my ($self, $to) = (@_);
-
-	$self->Subscription(
-		to   => $to,
-		type => 'subscribe',
-	);
-
-	$self->Subscription(
-		to   => $to,
-		type => 'subscribed',
-	);
-
-	return $self;
-});
-
-Alleria->extend(unsubscribe => sub {
-	my ($self, $to) = (@_);
-
-	$self->Subscription(
-		to   => $to,
-		type => 'unsubscribe',
-	);
-
-	$self->Subscription(
-		to   => $to,
-		type => 'unsubscribed',
-	);
-
-	return $self;
 });
 
 1;
--- plugins/subscription.pl
+++ plugins/subscription.pl
@@ -1,5 +1,7 @@
 use Alleria::Core 'strict';
 
+Alleria->load('presence');
+
 Alleria->focus(['presence::subscribe', 'presence::unsubscribe'], sub {
 	my ($self, $event, $args) = (@_);
 	my $presence = $args->[0];
@@ -7,11 +9,43 @@ Alleria->focus(['presence::subscribe', 'presence::unsubscribe'], sub {
 	my $method = (split '::', $event, 2)[1];
 
 	$self->$method($from)
-		if ($self->can('accessible') || sub { 1 })->({
+		if ($self->can('accessible') || sub { 1 })->($self, {
 			rule => 'actions',
 			from => $from,
 			name => $method,
 		});
+});
+
+Alleria->extend(subscribe => sub {
+	my ($self, $to) = (@_);
+
+	$self->Subscription(
+		to   => $to,
+		type => 'subscribe',
+	);
+
+	$self->Subscription(
+		to   => $to,
+		type => 'subscribed',
+	);
+
+	return $self;
+});
+
+Alleria->extend(unsubscribe => sub {
+	my ($self, $to) = (@_);
+
+	$self->Subscription(
+		to   => $to,
+		type => 'unsubscribe',
+	);
+
+	$self->Subscription(
+		to   => $to,
+		type => 'unsubscribed',
+	);
+
+	return $self;
 });
 
 1;
