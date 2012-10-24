0199bf011862e420d47966becc4a29d70014fe77
n4kz <nfokz@yandex.ru>
2012-10-25 01:04:01 +0400

    Chatstates events and pod for message plugin

--- plugins/message.pl
+++ plugins/message.pl
@@ -1,5 +1,4 @@
 use Alleria::Core 'strict';
-use Data::Dumper;
 
 my %fields = map { ("Get$_" => lc) } qw{ Body From Subject Type TimeStamp XML };
 
@@ -13,6 +12,16 @@ Alleria->focus(message => sub {
 	$message{'from'}     = $1;
 
 	given ($message{'type'}) {
+		when (m{(?:group)?chat}) {
+			# Fire chatstate event
+			$self->fire(join('::', $event, $1), [\%message]) if 
+				$message{'xml'} =~ m{((?:in)?active|composing|paused|gone) xmlns=.http://jabber.org/protocol/chatstates};
+
+			# Fall through if message also contains body
+			return unless $message{'body'};
+			continue;
+		}
+
 		when ('groupchat') {
 			$event .= '::groupchat';
 			$event .= '::system' unless $message{'resource'};
@@ -24,8 +33,6 @@ Alleria->focus(message => sub {
 
 		default {
 			undef $event;
-			warn 'message';
-			warn Dumper(\%message);
 		}
 	}
 
@@ -46,3 +53,57 @@ Alleria->extend(message => sub {
 });
 
 1;
+
+=pod
+
+=head1 NAME
+
+Message plugin for L<Alleria>
+
+=head1 SYNOPSIS
+
+	Alleria->load('message');
+
+	Alleria->focus('message::chat', sub {
+		my ($self, $event, $args) = @_;
+		my $message = $args[0];
+
+		$self->message({
+			to => $message->{'from'},
+			body => $message->{'body'},
+		});
+	});
+
+=head1 METHODS
+
+This plugin adds one method to Alleria class
+
+=head2 message
+
+	$self->message({
+		to   => 'test@example.com',
+		body => 'Sample message',
+		type => 'chat'
+	});
+
+=head1 EVENTS
+
+This plugin listens message event and fires the following ones
+
+=head2 General
+
+	message::chat
+	message::groupchat
+	message::groupchat::system
+	message::groupchat::delay
+	message::groupchat::system::delay
+
+=head2 Chatstates
+
+	message::active
+	message::inactive
+	message::composing
+	message::paused
+	message::gone
+
+=cut
